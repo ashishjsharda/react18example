a878ac1d529c5d268893dd3cfd73a737c317e628
Ashish Sharda <ashishjsharda@gmail.com>
2023-09-04 10:05:25 -0500

    add counter and update app.js

---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Counter(){
+  const [count, setCount] = React.useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+export default Counter;
